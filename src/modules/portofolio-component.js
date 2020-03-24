ad787bdb2fb5992b1dc8f6e82acc93ec93ccab29
hafifaroon <syaiful.noor.afifah@mail.ugm.ac.id>
2020-03-25 03:28:44 +0700

    Tugas 5 Routing tambah button

--- src/modules/portofolio-component.js
+++ src/modules/portofolio-component.js
@@ -8,21 +8,21 @@ import ButtonHome from '../common/component/button/button-home';
 const { Content } = Layout; // membuat konstanta content 
 const whyData = [
     {
-        // image: require(`../../../assets/images/picture.png`),
+        image: require(`../../src/assets/images/picture.png`),
         title: 'Grizzly',
-        link : "",
+        link : "https://youtu.be/AG-WwTdOC5I",
         description: "Despite my appearance and rather childish behavior, I am shown to be fairly knowledgeable about some things to help guide my brothers if they need help. I love my brothers unconditionally and would take a bullet for them."
     },
     {
-       // image: require(`../../../assets/images/panda.png`),
+        image: require(`../../src/assets/images/panda.png`),
         title: 'Panda',
-        link : "",
+        link : "https://youtu.be/r13rD5wiDjQ",
         description: "He's gentle, yet somewhat cowardly and insecure. \n He is probably the frailest of the three bears. \n He is also severely allergic to nut \n and gets embarrassed easily. His hobbies  consist \n of dating sites, social networking, anime, video \n games and art."
     },
     {
-       // image: require(`../../../assets/images/icebear.png`),
+        image: require(`../../src/assets/images/icebear.png`),
         title: 'Ice Bear',
-        link : "",
+        link : "https://youtu.be/u_yKAJjAkGI",
         description: "Ice Bear sleeps in the fridge and seems to love being on cold surfaces. He is also very interested in yoga, dancing, karate, and many other similar activities while he also spends a lot of time reading and tinkering with robotics."
     }
 ]
@@ -68,7 +68,15 @@ class PortofolioComponent extends Component{
                                                     <div className="mt-10" style={{maxWidth:'260px'}}>
                                                         {data.description}<br/><br/>
                                                         <a href={data.link} className="btn btn-primary"></a>
-                                                       {}
+                                                        <a href={data.link} target="_blank">
+                                                            <ButtonHome
+                                                                text="More"
+                                                                background="#a16348"
+                                                                textColor="#fff"
+                                                                className='button-participate'
+                                                                onClick = {data.link}
+                                                            />
+                                                        </a>
                                                     </div>
                                                 </div>
                                             </Col>

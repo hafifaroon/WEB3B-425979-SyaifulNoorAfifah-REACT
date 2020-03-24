ed2c936afb457faabd832b9b100b75989dc5fb38
hafifaroon <syaiful.noor.afifah@mail.ugm.ac.id>
2020-03-25 04:09:19 +0700

    Updates

---
+++ precache-manifest.41193bf12ef1d2b4797d58155db1d4af.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db6e6fe91e3b66cb4db047152145bc9",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/index.html"
+  },
+  {
+    "revision": "29979a20006a2feefe52",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "0b269e6c31b83d6d2f4d",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/css/main.7a887114.chunk.css"
+  },
+  {
+    "revision": "29979a20006a2feefe52",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/js/2.2a154198.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/js/2.2a154198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b269e6c31b83d6d2f4d",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/js/main.0a29e1e5.chunk.js"
+  },
+  {
+    "revision": "0d71ef118ef330042723",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/js/runtime-main.939730f9.js"
+  },
+  {
+    "revision": "ce3552ca054ead6c23015346aa1e10f4",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/media/icebear.ce3552ca.png"
+  },
+  {
+    "revision": "72109c5e9e97355823d2e8a4460ef216",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/media/panda.72109c5e.png"
+  },
+  {
+    "revision": "3a35c196b436cc8bf5a0a61035f6897b",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/media/picture.3a35c196.png"
+  },
+  {
+    "revision": "5d1fc817f86821b8f2cb4b07e72cb9c8",
+    "url": "/WEB3B-425979-SyaifulNoorAfifah-REACT/static/media/picture.5d1fc817.svg"
+  }
+]);
